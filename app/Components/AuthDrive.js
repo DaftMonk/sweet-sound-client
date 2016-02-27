80fcd573d980b299909b8b461ea5e5a83a86aa3a
Tyler Henkel <tylerhenkel@hotmail.com>
2016-02-27 03:18:26 -0700

    get files from google drive for sound folder

--- app/Components/AuthDrive.js
+++ app/Components/AuthDrive.js
@@ -1,6 +1,56 @@
+import Promise from 'bluebird';
+import React from 'react';
+import googleApiLoader from '../Lib/GoogleAPILoader';
+import _ from 'lodash';
 
-var React = require('react');
-var googleApiLoader = require('../Lib/GoogleAPILoader');
+var walkFilePages = function(request) {
+  return new Promise((resolve) => {
+    retrieveRecursiveFiles(request, [], resolve)
+  });
+};
+
+var retrieveRecursiveFiles = function(request, arr, done) {
+  return request.execute((resp) => {
+    arr = arr.concat(resp.items);
+    var nextPageToken = resp.nextPageToken;
+    if (nextPageToken) {
+      request = gapi.client.request({
+        'path': '/drive/v2/files',
+        'method': 'GET',
+        'pageToken': nextPageToken,
+        'params': {
+          'q': 'trashed = false'
+        }
+      });
+      retrieveRecursiveFiles(request, arr);
+    } else {
+      done(arr);
+    }
+  });
+}
+
+
+var retrieveAllFiles = function(folderId)  {
+  var initialRequest = gapi.client.request({
+    'path': '/drive/v2/files',
+    'method': 'GET',
+    'params': {
+      'q': "trashed = false and '" + folderId + "' in parents"
+    }
+  });
+  return walkFilePages(initialRequest);
+}
+
+function getSubFolders(folderId) {
+  var initialRequest = gapi.client.request({
+    'path': '/drive/v2/files',
+    'method': 'GET',
+    'params': {
+      'q': "trashed = false and '" + folderId + "' in parents and mimeType='application/vnd.google-apps.folder'"
+    }
+  });
+  return walkFilePages(initialRequest);
+}
 
 module.exports = React.createClass({
 
@@ -14,16 +64,43 @@ module.exports = React.createClass({
       googleApiLoader.apiReady.then(() => {
         _this.setState({isAuthorized: true});
 
-        return googleApiLoader.execute(() => {
-          return gapi.client.drive.files.list({
-            'pageSize': 10,
-            'fields': "nextPageToken, files(id, name)"
+        return gapi.client.drive.files.list({
+          'q': "mimeType='application/vnd.google-apps.folder' and name='sounds'",
+          'pageSize': 10,
+          'fields': "nextPageToken, files(id, name)"
+        }).execute((resp) => {
+          var folders = resp.files;
+          var soundFolderId = folders[0].id;
+          var tree = {
+          };
+          getSubFolders(soundFolderId).then((folders) => {
+            let i = 0;
+            return Promise.map(folders, function(folder) {
+              (function(index) {
+                setTimeout(function() {
+                  retrieveAllFiles(folder.id).then(function (files) {
+                    tree[folder.title] = folder;
+                    tree[folder.title]._files = files;
+                    console.log(tree);
+                  })
+                }, index * 100);
+              })(i);
+              i++;
+            });
           });
-        }).then(resp => {
-          var files = resp.files;
-          console.log(files);
-          _this.setState({files: files});
         });
+        //.then(resp => {
+
+        //  return googleApiLoader.execute(() => {
+        //    return gapi.client.drive.files.list({
+        //      'q': "mimeType='audio/mpeg'"
+        //    });
+        //  }).then(resp => {
+        //    var files = resp.files;
+        //    console.log(resp);
+        //    _this.setState({files: files});
+        //  });
+        //});
       });
     },
 
